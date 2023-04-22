3984856cb2f9e8978549699a94138593c49b0b5a
vaycore <hxhvay@126.com>
2023-04-22 14:45:41 +0800

    新增字典管理；在配置中初始化字典管理、指纹管理；将所有字典相关方法迁移到字典管理

--- src/main/java/burp/vaycore/onescan/common/Config.java
+++ src/main/java/burp/vaycore/onescan/common/Config.java
@@ -5,14 +5,16 @@ import burp.vaycore.common.log.Logger;
 import burp.vaycore.common.utils.FileUtils;
 import burp.vaycore.common.utils.PathUtils;
 import burp.vaycore.common.utils.StringUtils;
-import burp.vaycore.onescan.ui.payloadlist.PayloadItem;
-import burp.vaycore.onescan.ui.payloadlist.PayloadRule;
-import burp.vaycore.onescan.ui.payloadlist.SimplePayloadList;
+import burp.vaycore.onescan.manager.FpManager;
+import burp.vaycore.onescan.manager.WordlistManager;
+import burp.vaycore.onescan.ui.widget.payloadlist.PayloadItem;
+import burp.vaycore.onescan.ui.widget.payloadlist.PayloadRule;
+import burp.vaycore.onescan.ui.widget.payloadlist.SimplePayloadList;
+import com.google.gson.internal.LinkedTreeMap;
 
 import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * 配置类
@@ -20,104 +22,136 @@ import java.util.HashMap;
  * Created by vaycore on 2022-08-19.
  */
 public class Config {
-
     public static final String KEY_VERSION = "version";
-    public static final String KEY_PAYLOAD_LIST = "payload-list";
     public static final String KEY_PAYLOAD_PROCESS_LIST = "payload-process-list";
-    public static final String KEY_HEADER_LIST = "header-list";
-    public static final String KEY_UA_LIST = "user-agent-list";
-    public static final String KEY_WHITE_LIST = "whitelist";
-    public static final String KEY_BLACK_LIST = "blacklist";
     public static final String KEY_QPS_LIMIT = "qps-limit";
     public static final String KEY_WEB_NAME_COLLECT_PATH = "web-name-collect-path";
     public static final String KEY_JSON_FIELD_COLLECT_PATH = "json-field-collect-path";
     public static final String KEY_EXCLUDE_SUFFIX = "exclude-suffix";
     public static final String KEY_HAE_PLUGIN_PATH = "hae-plugin-path";
     public static final String KEY_INCLUDE_METHOD = "include-method";
-    public static final String KEY_EXCLUDE_HEADER = "exclude-header";
-
-    private static ConfigManager mConfigManager;
-    private static String mConfigPath;
+    public static final String KEY_WORDLIST_PATH = "dict-path";
+    private static ConfigManager sConfigManager;
+    private static String sConfigPath;
 
     public static void init() {
-        // 初始化配置模块
-        mConfigPath = getWorkDir() + "config.json";
-        mConfigManager = new ConfigManager(mConfigPath);
-        // 初始值配置
-        initDefaultConfig(KEY_VERSION, Constants.PLUGIN_VERSION);
-        initDefaultResourceConfig(KEY_PAYLOAD_LIST, "payload.txt");
-        initDefaultResourceConfig(KEY_HEADER_LIST, "header.txt");
-        initDefaultResourceConfig(KEY_UA_LIST, "user_agent.txt");
-        initDefaultResourceConfig(KEY_WHITE_LIST, "whitelist.txt");
-        initDefaultResourceConfig(KEY_BLACK_LIST, "blacklist.txt");
-        initDefaultConfig(KEY_QPS_LIMIT, "1024");
-        initDefaultConfig(KEY_WEB_NAME_COLLECT_PATH, getWorkDir() + "web_name.txt");
-        initDefaultConfig(KEY_JSON_FIELD_COLLECT_PATH, getWorkDir() + "json-fields");
+        sConfigPath = getWorkDir() + "config.json";
+        sConfigManager = new ConfigManager(sConfigPath);
+        initDefaultConfig(Config.KEY_VERSION, Constants.PLUGIN_VERSION);
+        initDefaultConfig(Config.KEY_QPS_LIMIT, "1024");
+        initDefaultConfig(Config.KEY_WEB_NAME_COLLECT_PATH, getWorkDir() + "web_name.txt");
+        initDefaultConfig(Config.KEY_JSON_FIELD_COLLECT_PATH, getWorkDir() + "json-fields");
         initDefaultConfig(KEY_EXCLUDE_SUFFIX, "3g2|3gp|7z|aac|abw|aif|aifc|aiff|arc|au|avi|azw|bin|bmp|bz|" +
                 "bz2|cmx|cod|csh|css|csv|doc|docx|eot|epub|gif|gz|ico|ics|ief|jar|jfif|jpe|jpeg|jpg|m3u|mid|midi|" +
                 "mjs|mp2|mp3|mpa|mpe|mpeg|mpg|mpkg|mpp|mpv2|odp|ods|odt|oga|ogv|ogx|otf|pbm|pdf|pgm|png|pnm|ppm|" +
                 "ppt|pptx|ra|ram|rar|ras|rgb|rmi|rtf|snd|svg|swf|tar|tif|tiff|ttf|vsd|wav|weba|webm|webp|woff|" +
                 "woff2|xbm|xls|xlsx|xpm|xul|xwd|zip|zip");
-        initDefaultConfig(KEY_INCLUDE_METHOD, "GET|POST");
-        initDefaultResourceConfig(KEY_EXCLUDE_HEADER, "exclude_header.txt");
+        initDefaultConfig(Config.KEY_INCLUDE_METHOD, "GET|POST");
+        initDefaultConfig(Config.KEY_WORDLIST_PATH, getWorkDir() + "wordlist");
+        // 初始化字典管理
+        WordlistManager.init(get(Config.KEY_WORDLIST_PATH));
+        // 初始化指纹管理
+        initFpManager();
         // 版本更新处理
         onVersionUpgrade();
         // 预加载一些需要实例化成对象的配置
         onPreloadConfig();
     }
 
+    private static void initFpManager() {
+        String path = getWorkDir() + "fingerprint.json";
+        if (!FileUtils.isFile(path)) {
+            InputStream is = Config.class.getClassLoader().getResourceAsStream("fingerprint.json");
+            String content = FileUtils.readStreamToString(is);
+            FileUtils.writeFile(path, content);
+        }
+
+        FpManager.init(path);
+    }
+
     private static void onVersionUpgrade() {
-        // 版本更新，配置也需要更新，更新后保留旧配置
-        String version = Config.getVersion();
-        if (!version.equals(Constants.PLUGIN_VERSION)) {
-            // 在配置文件中更新版本号
-            putVersion(Constants.PLUGIN_VERSION);
-            // 如果包含{{mdomain}}，将整个配置文件的{{mdomain}}替换为{{domain.name}}
-            String configJson = FileUtils.readFileToString(mConfigPath);
-            if (configJson.contains("{{mdomain}}")) {
-                configJson = configJson.replace("{{mdomain}}", "{{domain.name}}");
-                boolean state = FileUtils.writeFile(mConfigPath, configJson);
-                if (state) {
-                    Logger.info("Replace all {{mdomain}} to {{domain.name}} ok!");
-                    init();
-                }
+        String version = getVersion();
+        if (!version.equals("1.0.0")) {
+            putVersion("1.0.0");
+            upgradeDomain();
+            upgradeRemoveHeaderList();
+            upgradeWordlist();
+        }
+
+    }
+
+    private static void upgradeDomain() {
+        String configJson = FileUtils.readFileToString(sConfigPath);
+        if (configJson.contains("{{mdomain}}")) {
+            configJson = configJson.replace("{{mdomain}}", "{{domain.name}}");
+            boolean state = FileUtils.writeFile(sConfigPath, configJson);
+            if (state) {
+                Logger.info("Replace all {{mdomain}} to {{domain.name}} ok!");
+                init();
             }
-            // 将remove-header-list配置项迁移到新字段
-            if (mConfigManager.hasKey("remove-header-list")) {
-                ArrayList<String> list = getList("remove-header-list");
-                putList(KEY_EXCLUDE_HEADER, list);
-                mConfigManager.remove("remove-header-list");
-            }
+        }
+    }
+
+    private static void upgradeRemoveHeaderList() {
+        // 将remove-header-list配置项迁移到新字段
+        if (hasKey("remove-header-list")) {
+            ArrayList<String> list = getList("remove-header-list");
+            WordlistManager.putList(WordlistManager.KEY_EXCLUDE_HEADERS, list);
+            sConfigManager.remove("remove-header-list");
+        }
+    }
+
+    private static void upgradeWordlist() {
+        ArrayList<String> list;
+        if (hasKey("payload-list")) {
+            list = getList("payload-list");
+            WordlistManager.putList(WordlistManager.KEY_PAYLOAD, list);
+            sConfigManager.remove("payload-list");
+        }
+
+        if (hasKey("header-list")) {
+            list = getList("header-list");
+            WordlistManager.putList(WordlistManager.KEY_HEADERS, list);
+            sConfigManager.remove("header-list");
+        }
+
+        if (hasKey("user-agent-list")) {
+            list = getList("user-agent-list");
+            WordlistManager.putList(WordlistManager.KEY_USER_AGENT, list);
+            sConfigManager.remove("user-agent-list");
+        }
+
+        if (hasKey("whitelist")) {
+            list = getList("whitelist");
+            WordlistManager.putList(WordlistManager.KEY_WHITE_HOST, list);
+            sConfigManager.remove("whitelist");
+        }
+
+        if (hasKey("blacklist")) {
+            list = getList("blacklist");
+            WordlistManager.putList(WordlistManager.KEY_BLACK_HOST, list);
+            sConfigManager.remove("blacklist");
+        }
+
+        if (hasKey("exclude-header")) {
+            list = getList("exclude-header");
+            WordlistManager.putList(WordlistManager.KEY_EXCLUDE_HEADERS, list);
+            sConfigManager.remove("exclude-header");
         }
     }
 
     private static void onPreloadConfig() {
-        // 处理 Payload Process 列表配置
         preparePayloadProcessList();
     }
 
-    public static String getConfigPath() {
-        return mConfigPath;
-    }
-
-    private static void initDefaultResourceConfig(String key, String resName) {
-        if (mConfigManager.hasKey(key)) {
-            return;
-        }
-        InputStream is = Config.class.getClassLoader().getResourceAsStream(resName);
-        ArrayList<String> list = FileUtils.readStreamToList(is);
-        mConfigManager.put(key, list);
-    }
-
     private static void initDefaultConfig(String key, String defValue) {
-        if (mConfigManager.hasKey(key)) {
-            return;
+        if (!hasKey(key)) {
+            sConfigManager.put(key, defValue);
         }
-        mConfigManager.put(key, defValue);
     }
 
     private static void checkInit() {
-        if (mConfigManager == null) {
+        if (sConfigManager == null) {
             throw new IllegalStateException("Config class is not initialize");
         }
     }
@@ -134,38 +168,17 @@ public class Config {
         put(KEY_VERSION, version);
     }
 
-    public static ArrayList<String> getPayloadList() {
-        return Config.getList(Config.KEY_PAYLOAD_LIST);
-    }
-
-    public static ArrayList<String> getWhitelist() {
-        return Config.getList(Config.KEY_WHITE_LIST);
-    }
-
-    public static ArrayList<String> getBlacklist() {
-        return Config.getList(Config.KEY_BLACK_LIST);
-    }
-
-    public static ArrayList<String> getHeaderList() {
-        return Config.getList(Config.KEY_HEADER_LIST);
-    }
-
     public static String getFilePath(String key) {
         return getFilePath(key, false);
     }
 
     public static String getFilePath(String key, boolean isDir) {
         checkInit();
-        String path = mConfigManager.get(key);
+        String path = sConfigManager.get(key);
         if (StringUtils.isEmpty(path)) {
             return path;
         }
-        File dir;
-        if (isDir) {
-            dir = new File(path);
-        } else {
-            dir = new File(path).getParentFile();
-        }
+        File dir = isDir ? new File(path) : new File(path).getParentFile();
         if (!dir.exists()) {
             boolean mkdirs = dir.mkdirs();
             Logger.debug("Config item path mkdirs: %s", mkdirs);
@@ -175,49 +188,46 @@ public class Config {
 
     public static void put(String key, String text) {
         checkInit();
-        mConfigManager.put(key, text);
+        sConfigManager.put(key, text);
     }
 
     public static void put(String key, Object obj) {
         checkInit();
-        mConfigManager.put(key, obj);
-    }
-
-    public static void putList(String key, ArrayList<String> list) {
-        checkInit();
-        if (list == null) {
-            return;
-        }
-        // 添加到缓存
-        ArrayList<String> newList = new ArrayList<>(list);
-        mConfigManager.put(key, newList);
+        sConfigManager.put(key, obj);
     }
 
     public static String get(String key) {
         checkInit();
-        return mConfigManager.get(key);
+        return sConfigManager.get(key);
     }
 
     public static ArrayList<String> getList(String key) {
         checkInit();
-        return mConfigManager.getList(key);
+        return sConfigManager.getList(key);
+    }
+
+    public static boolean hasKey(String key) {
+        checkInit();
+        return sConfigManager.hasKey(key);
     }
 
     private static void preparePayloadProcessList() {
-        ArrayList<HashMap<String, Object>> mapItems = mConfigManager.get(Config.KEY_PAYLOAD_PROCESS_LIST);
+        ArrayList<LinkedTreeMap<String, Object>> mapItems = sConfigManager.get(Config.KEY_PAYLOAD_PROCESS_LIST);
         ArrayList<PayloadItem> result = new ArrayList<>();
         if (mapItems != null && !mapItems.isEmpty()) {
-            // 转换HashMap数据为PayloadItem对象
-            for (HashMap<String, Object> mapItem : mapItems) {
+            // 转换 LinkedTreeMap 数据为 PayloadItem 对象
+            for (LinkedTreeMap<String, Object> mapItem : mapItems) {
                 PayloadItem item = new PayloadItem();
-                item.setId((Integer) mapItem.get("id"));
+                Double id = (Double) mapItem.get("id");
+                item.setId(id.intValue());
                 item.setEnabled((Boolean) mapItem.get("enabled"));
-                item.setScope((Integer) mapItem.get("scope"));
+                Double scope = (Double) mapItem.get("scope");
+                item.setScope(scope.intValue());
                 PayloadRule rule = SimplePayloadList.getPayloadRuleByType((String) mapItem.get("ruleType"));
                 if (rule == null) {
                     continue;
                 }
-                HashMap<String, Object> ruleMap = (HashMap<String, Object>) mapItem.get("rule");
+                LinkedTreeMap<String, Object> ruleMap = (LinkedTreeMap<String, Object>) mapItem.get("rule");
                 ArrayList<String> ruleParamValues = (ArrayList<String>) ruleMap.get("paramValues");
                 for (int j = 0; j < rule.paramCount(); j++) {
                     String paramValue = ruleParamValues.get(j);
@@ -227,12 +237,11 @@ public class Config {
                 result.add(item);
             }
         }
-        // 将列表缓存
         put(Config.KEY_PAYLOAD_PROCESS_LIST, result);
     }
 
     public static ArrayList<PayloadItem> getPayloadProcessList() {
         checkInit();
-        return mConfigManager.get(Config.KEY_PAYLOAD_PROCESS_LIST);
+        return sConfigManager.get(Config.KEY_PAYLOAD_PROCESS_LIST);
     }
 }
---
+++ src/main/java/burp/vaycore/onescan/manager/WordlistManager.java
@@ -0,0 +1,176 @@
+package burp.vaycore.onescan.manager;
+
+import burp.vaycore.common.log.Logger;
+import burp.vaycore.common.utils.FileUtils;
+import burp.vaycore.common.utils.StringUtils;
+import burp.vaycore.onescan.common.Config;
+
+import java.io.File;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 字典管理
+ * <p>
+ * Created by vaycore on 2023-04-21.
+ */
+public class WordlistManager {
+    public static final String KEY_PAYLOAD = "payload";
+    public static final String KEY_HEADERS = "headers";
+    public static final String KEY_USER_AGENT = "user-agent";
+    public static final String KEY_WHITE_HOST = "white-host";
+    public static final String KEY_BLACK_HOST = "black-host";
+    public static final String KEY_EXCLUDE_HEADERS = "exclude-headers";
+    private static String sWordlistDir;
+
+    public static void init(String path) {
+        init(path, false);
+    }
+
+    public static void init(String path, boolean reInitFile) {
+        if (!FileUtils.exists(path)) {
+            FileUtils.mkdirs(path);
+        }
+
+        if (FileUtils.isDir(path)) {
+            sWordlistDir = path;
+            initDirs();
+            initDefaultWordlist(WordlistManager.KEY_PAYLOAD, "payload.txt", reInitFile);
+            initDefaultWordlist(WordlistManager.KEY_HEADERS, "header.txt", reInitFile);
+            initDefaultWordlist(WordlistManager.KEY_USER_AGENT, "user_agent.txt", reInitFile);
+            initDefaultWordlist(WordlistManager.KEY_WHITE_HOST, "whitelist.txt", reInitFile);
+            initDefaultWordlist(WordlistManager.KEY_BLACK_HOST, "blacklist.txt", reInitFile);
+            initDefaultWordlist(WordlistManager.KEY_EXCLUDE_HEADERS, "exclude_header.txt", reInitFile);
+        } else {
+            throw new IllegalArgumentException("Wordlist path not found.");
+        }
+    }
+
+    private static void initDirs() {
+        Field[] fields = WordlistManager.class.getDeclaredFields();
+        for (Field field : fields) {
+            String name = field.getName();
+            if (name.startsWith("KEY_")) {
+                try {
+                    String value = (String) field.get(null);
+                    String path = sWordlistDir + File.separator + value;
+                    if (!FileUtils.isDir(path)) {
+                        boolean state = FileUtils.mkdirs(path);
+                        if (state) {
+                            Logger.debug("initDirs: %s ok!", path);
+                        }
+                    }
+                } catch (IllegalAccessException e) {
+                    // ignored
+                }
+            }
+        }
+    }
+
+    private static void initDefaultWordlist(String key, String resName, boolean reInitFile) {
+        if (reInitFile || !Config.hasKey(key)) {
+            List<String> itemList = getItemList(key);
+            String defaultItem = "default";
+            if (!itemList.isEmpty()) {
+                defaultItem = itemList.contains("default") ? "default" : (String) itemList.get(0);
+            }
+
+            Config.put(key, defaultItem);
+            if (!wordlistFileExists(key)) {
+                InputStream is = Config.class.getClassLoader().getResourceAsStream(resName);
+                ArrayList<String> list = FileUtils.readStreamToList(is);
+                putList(key, list);
+            }
+
+        }
+    }
+
+    public static String getItem(String key) {
+        String item = Config.get(key);
+        if (StringUtils.isEmpty(item)) {
+            item = "default";
+        }
+        return item;
+    }
+
+    public static void putItem(String key, String item) {
+        Config.put(key, item);
+    }
+
+    public static List<String> getList(String key) {
+        checkInit();
+        String item = getItem(key);
+        String path = sWordlistDir + File.separator + key + File.separator + item + ".txt";
+        ArrayList<String> list = FileUtils.readFileToList(path);
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+
+        return list;
+    }
+
+    public static void putList(String key, List<String> list) {
+        checkInit();
+        if (list == null) {
+            return;
+        }
+        String item = getItem(key);
+        String path = sWordlistDir + File.separator + key + File.separator + item + ".txt";
+        String content = StringUtils.join(list, "\n");
+        FileUtils.writeFile(path, content);
+    }
+
+    public static List<String> getItemList(String key) {
+        checkInit();
+        String path = sWordlistDir + File.separator + key;
+        File file = new File(path);
+        File[] files = file.listFiles((pathname) -> pathname.getName().endsWith(".txt") && pathname.isFile());
+        List<String> result = new ArrayList<>();
+        if (files == null || files.length == 0) {
+            return result;
+        }
+        for (File items : files) {
+            String itemPath = items.getName().replace(".txt", "");
+            result.add(itemPath);
+        }
+        return result;
+    }
+
+    public static boolean wordlistFileExists(String key) {
+        String item = getItem(key);
+        String path = sWordlistDir + File.separator + key + File.separator + item + ".txt";
+        return FileUtils.isFile(path);
+    }
+
+    private static void checkInit() {
+        if (StringUtils.isEmpty(sWordlistDir) || !FileUtils.isDir(sWordlistDir)) {
+            throw new IllegalArgumentException("WordlistManager no init.");
+        }
+    }
+
+    public static List<String> getPayload() {
+        return getList(WordlistManager.KEY_PAYLOAD);
+    }
+
+    public static List<String> getHeader() {
+        return getList(WordlistManager.KEY_HEADERS);
+    }
+
+    public static List<String> getUserAgent() {
+        return getList(WordlistManager.KEY_USER_AGENT);
+    }
+
+    public static List<String> getWhiteHost() {
+        return getList(WordlistManager.KEY_WHITE_HOST);
+    }
+
+    public static List<String> getBlackHost() {
+        return getList(WordlistManager.KEY_BLACK_HOST);
+    }
+
+    public static List<String> getExcludeHeader() {
+        return getList(WordlistManager.KEY_EXCLUDE_HEADERS);
+    }
+}
