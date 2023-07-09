3417adbbe65f2c2d819a1d170f5c116e3a2c2038
vaycore <hxhvay@126.com>
2023-07-09 16:50:06 +0800

    更新版本号为1.1.0

--- src/main/java/burp/vaycore/onescan/common/Constants.java
+++ src/main/java/burp/vaycore/onescan/common/Constants.java
@@ -9,7 +9,7 @@ public interface Constants {
 
     // 插件信息
     String PLUGIN_NAME = "OneScan";
-    String PLUGIN_VERSION = "1.0.4";
+    String PLUGIN_VERSION = "1.1.0";
     boolean DEBUG = false;
 
     // 插件启动显示的信息
