8201ae7620d0b502174aef6953c4a139988f73b6
vaycore <25676623+vaycore@users.noreply.github.com>
2025-06-06 21:03:50 +0800

    修改 'exclude-suffix' 默认配置项，删除其中重复的 'zip' 值

--- extender/src/main/java/burp/vaycore/onescan/common/Config.java
+++ extender/src/main/java/burp/vaycore/onescan/common/Config.java
@@ -69,7 +69,7 @@ public class Config {
                 "bz2|cmx|cod|csh|css|csv|doc|docx|eot|epub|gif|gz|ico|ics|ief|jar|jfif|jpe|jpeg|jpg|m3u|mid|midi|" +
                 "mjs|mp2|mp3|mpa|mpe|mpeg|mpg|mpkg|mpp|mpv2|odp|ods|odt|oga|ogv|ogx|otf|pbm|pdf|pgm|png|pnm|ppm|" +
                 "ppt|pptx|ra|ram|rar|ras|rgb|rmi|rtf|snd|svg|swf|tar|tif|tiff|ttf|vsd|wav|weba|webm|webp|woff|" +
-                "woff2|xbm|xls|xlsx|xpm|xul|xwd|zip|zip");
+                "woff2|xbm|xls|xlsx|xpm|xul|xwd|zip");
         initDefaultConfig(Config.KEY_INCLUDE_METHOD, "GET|POST");
         initDefaultConfig(Config.KEY_WORDLIST_PATH, getWorkDir() + "wordlist");
         // 默认开关配置
